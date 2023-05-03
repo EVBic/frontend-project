fe57f19664c857174d0df06aa45f1cbf6bc05d76
EVBic <129067154+EVBic@users.noreply.github.com>
2023-05-03 08:48:25 +0000

    tn3

--- Timer/src/main.js
+++ Timer/src/main.js
@@ -1,8 +1,27 @@
 import "./style.css";
+function formatTime(seconds) {
+  const reminderSeconds = seconds % 60;
+  const minutes = (seconds - reminderSeconds) / 60;
+  const formattedSeconds =
+    reminderSeconds < 10 ? `0${reminderSeconds}` : reminderSeconds;
+  return `${minutes}:${formattedSeconds}`;
+}
 
 const root = document.querySelector("#app");
-let counter = 0;
-setInterval(()=> {
-  root.innerText = `${counter}`;
-  counter++;
-},1000);
+const startBtn = document.getElementById("start-timer");
+startBtn.addEventListener("click", startTimer);
+function startTimer() {
+  let counter = 10;
+  root.innerText = formatTime(counter);
+  startBtn.disabled = true;
+  document.body.classList.add("bg-green-400");
+  const timer = setInterval(() => {
+    counter--;
+    root.innerText = formatTime(counter);
+    if (counter === 0) {
+      clearInterval(timer);
+      document.body.classList.add("bg-red-400");
+      startBtn.disabled = false;
+    }
+  }, 1000);
+}
